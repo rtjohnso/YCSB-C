bcbd057bae21388f082c4dc61d68da32198ac7ad
deukyeon <deukyeon.hwang@gmail.com>
2022-10-19 21:43:05 -0700

    added a script to parse results and plot

--- run_splinterdb.sh
+++ run_splinterdb.sh
@@ -12,14 +12,22 @@ THREADS=(1 2 4 8)
 
 echo "Run for the uniform distribution"
 
+OUT=uniform.out
+
+rm -f $OUT
+
 for t in ${THREADS[@]}
 do
-  bash run_individual.sh $DB $t uniform
+  bash run_individual.sh $DB $t uniform >> $OUT 2>&1
 done
+
+OUT=zipf.out
+
+rm -f $OUT
 
 echo "Run for the zipfian distribution"
 
 for t in ${THREADS[@]}
 do
-  bash run_individual.sh $DB $t zipfian
+  bash run_individual.sh $DB $t zipfian >> $OUT 2>&1
 done
