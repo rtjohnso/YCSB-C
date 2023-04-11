79caba18015c6563927db2240cd83e2b40fd59e0
Alex Conway <aconway@vmware.com>
2023-04-11 07:29:25 -0700

    Add drop caches to run_lookup.sh

--- run_lookup.sh
+++ run_lookup.sh
@@ -13,20 +13,44 @@ set -x
 
 cgexec -g memory:benchmark ./ycsbc -db rocksdb -L ${WORKLOADDIR}/load.spec -p rocksdb.database_filename /mnt/benchmark/rocksdb -threads 14 -p rocksdb.config_file rocks.ini -p rocksdb.options.max_background_jobs 20 -p rocksdb.cache_size $CACHESIZEBYTES
 
+sync
+echo 3 > /proc/sys/vm/drop_caches
+iostat -m | grep nvme
+
 THREADS=8
 cgexec -g memory:benchmark ./ycsbc -db rocksdb -P ${WORKLOADDIR}/load.spec -W ${WORKLOADDIR}/${WORKLOADFILE} -p rocksdb.database_filename /mnt/benchmark/rocksdb -threads $THREADS -p rocksdb.config_file rocks.ini -p rocksdb.options.max_background_jobs 20 -p rocksdb.cache_size $CACHESIZEBYTES > rocks_lookup_${WORKLOAD}_${MEMSIZE}GiB 2>&1
+
+sync
+echo 3 > /proc/sys/vm/drop_caches
+iostat -m | grep nvme
 
 THREADS=12
 cgexec -g memory:benchmark ./ycsbc -db rocksdb -P ${WORKLOADDIR}/load.spec -W ${WORKLOADDIR}/${WORKLOADFILE} -p rocksdb.database_filename /mnt/benchmark/rocksdb -threads $THREADS -p rocksdb.config_file rocks.ini -p rocksdb.options.max_background_jobs 20 -p rocksdb.cache_size $CACHESIZEBYTES >> rocks_lookup_${WORKLOAD}_${MEMSIZE}GiB 2>&1
 
+sync
+echo 3 > /proc/sys/vm/drop_caches
+iostat -m | grep nvme
+
 THREADS=16
 cgexec -g memory:benchmark ./ycsbc -db rocksdb -P ${WORKLOADDIR}/load.spec -W ${WORKLOADDIR}/${WORKLOADFILE} -p rocksdb.database_filename /mnt/benchmark/rocksdb -threads $THREADS -p rocksdb.config_file rocks.ini -p rocksdb.options.max_background_jobs 20 -p rocksdb.cache_size $CACHESIZEBYTES >> rocks_lookup_${WORKLOAD}_${MEMSIZE}GiB 2>&1
+
+sync
+echo 3 > /proc/sys/vm/drop_caches
+iostat -m | grep nvme
 
 THREADS=20
 cgexec -g memory:benchmark ./ycsbc -db rocksdb -P ${WORKLOADDIR}/load.spec -W ${WORKLOADDIR}/${WORKLOADFILE} -p rocksdb.database_filename /mnt/benchmark/rocksdb -threads $THREADS -p rocksdb.config_file rocks.ini -p rocksdb.options.max_background_jobs 20 -p rocksdb.cache_size $CACHESIZEBYTES >> rocks_lookup_${WORKLOAD}_${MEMSIZE}GiB 2>&1
 
+sync
+echo 3 > /proc/sys/vm/drop_caches
+iostat -m | grep nvme
+
 THREADS=24
 cgexec -g memory:benchmark ./ycsbc -db rocksdb -P ${WORKLOADDIR}/load.spec -W ${WORKLOADDIR}/${WORKLOADFILE} -p rocksdb.database_filename /mnt/benchmark/rocksdb -threads $THREADS -p rocksdb.config_file rocks.ini -p rocksdb.options.max_background_jobs 20 -p rocksdb.cache_size $CACHESIZEBYTES >> rocks_lookup_${WORKLOAD}_${MEMSIZE}GiB 2>&1
+
+sync
+echo 3 > /proc/sys/vm/drop_caches
+iostat -m | grep nvme
 
 THREADS=28
 cgexec -g memory:benchmark ./ycsbc -db rocksdb -P ${WORKLOADDIR}/load.spec -W ${WORKLOADDIR}/${WORKLOADFILE} -p rocksdb.database_filename /mnt/benchmark/rocksdb -threads $THREADS -p rocksdb.config_file rocks.ini -p rocksdb.options.max_background_jobs 20 -p rocksdb.cache_size $CACHESIZEBYTES >> rocks_lookup_${WORKLOAD}_${MEMSIZE}GiB 2>&1
